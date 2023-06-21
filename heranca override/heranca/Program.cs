fff7901271a289e98c1541d721a38e748e1da72a
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-06-21 11:20:04 -0300

    anotacoes sobre polimorfismo

--- heranca override/heranca/Program.cs
+++ heranca override/heranca/Program.cs
@@ -1,5 +1,7 @@
 ﻿using heranca.Entities;
 
+//oque estamos fazendo abaixo é u exemplo de polimorfismo, o compilador não irá saber as conta1,2,3 são de tipos diferentes, só irá saber que são instancias
+//do tipo Account que possuem o método Withdraw, essa facilidade de podermos instanciarmos uma subclasse em uma variavel do tipo da superclasse é um tipo de polimorfismo.
 Account conta1 = new Account(1050, "gabriel", 850);
 Account conta2 = new SavingAccount(942, "dsm", 850, 2);
 Account conta3 = new BusinessAccount(6481, "leoneip", 850, 60);
