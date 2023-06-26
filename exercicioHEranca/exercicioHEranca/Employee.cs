061155c7b93daeb7becca7c0fe16f95793f55f74
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-06-26 11:06:27 -0300

    startig a new project about heranca

---
+++ exercicioHEranca/exercicioHEranca/Employee.cs
@@ -0,0 +1,22 @@
+﻿namespace exercicioHEranca
+{
+    internal class Employee
+    {
+        public Employee() { }
+        public Employee(string name, int hours, double valuePerHour)
+        {
+            this.name = name;
+            this.hours = hours;
+            this.valuePerHour = valuePerHour;
+        }
+
+        public string name;
+        public int hours;
+        public double valuePerHour;
+
+        public virtual double payment()
+        {
+            return valuePerHour * hours;
+        }
+    }
+}
---
+++ exercicioHEranca/exercicioHEranca/OutsourceEmployee.cs
@@ -0,0 +1,24 @@
+﻿namespace exercicioHEranca
+{
+    //utilizando o fundamento de sealed na classe
+    internal sealed class OutsourceEmployee : Employee
+    {
+        double AdditionalCharge;
+
+        public OutsourceEmployee() { }
+
+        //reutilizando o construtor da superclasse com o base
+        public OutsourceEmployee(string name, int hours, double valuePerHour, double additionalCharge) : base(name, hours, valuePerHour)
+        {
+            AdditionalCharge = additionalCharge;
+        }
+
+        //utilizando o fundamento do override no método payment com sealed
+        public sealed override double payment()
+        {
+            double basePayment = base.payment();
+            return basePayment + AdditionalCharge * 1.1;
+        }
+
+    }
+}
