abc3e913c9d137ab2eb66f12052abcb61e572701
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-06-30 14:58:54 -0300

    new abstract project

---
+++ abstractProject/abstractProject/Entities/LegalPerson.cs
@@ -0,0 +1,10 @@
+﻿namespace AbstractProject.Entities
+{
+    internal class LegalPerson
+    {
+        internal string Name { get; set; }
+        internal double AnualIncome { get; set; }
+        internal int EmployeeQuantity { get; set; }
+
+    }
+}
---
+++ abstractProject/abstractProject/Entities/NaturalPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractProject.Entities
+{
+    internal class NaturalPerson
+    {
+        internal string Name { get; set; }
+        internal double AnualIncome { get; set; }
+        internal double HealthSpending { get; set; }
+    }
+}
