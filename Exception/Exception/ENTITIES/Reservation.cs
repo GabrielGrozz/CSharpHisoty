5c9a40d0fe1b01aff4ac7398d850457f3068f27e
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-07-07 16:47:10 -0300

    finished project, but need more observations

--- Exception/Exception/ENTITIES/Reservation.cs
+++ Exception/Exception/ENTITIES/Reservation.cs
@@ -1,17 +1,24 @@
-﻿using Exception.ENTITIES
+﻿using Exception.ENTITIES.EXCEPTIONS;
 
 namespace Exception.ENTITIES
 {
     internal class Reservation
     {
-        internal int RoomNumver { get; set; }
+        internal int RoomNumber { get; set; }
         internal DateTime CheckIn { get; set; }
         internal DateTime CheckOut { get; set; }
 
         public Reservation() { }
-        public Reservation(int roomNumver, DateTime checkIn, DateTime checkOut)
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
         {
-            RoomNumver = roomNumver;
+            //podemos tratar excessões também no construtor
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException("a data de checkOut deve ser para depois da data de checkIn");
+            }
+
+            RoomNumber = roomNumber;
             CheckIn = checkIn;
             CheckOut = checkOut;
         }
@@ -25,13 +32,29 @@ namespace Exception.ENTITIES
             return (int)duration.TotalDays;
         }
 
+        //esse é o nosso método para quando formos atualizar a reserva, nele terá varios testes para ver se estamos passando os valores corretos
         internal void UpdateDates(DateTime checkin, DateTime checkout)
         {
             DateTime now = DateTime.Now;
             //basicamente se o checkin ou checkout forem de dias anteriores a agora lançaremos uma Exception
-            if (CheckIn < now || CheckOut < now) {
-                throw new Dom
+            if (checkin < now || checkout < now)
+            {
+                //o throw serve para lansar algo, no exemplo abaixo estamos lansando uma instancia de uma excessão, isso irá fazer com que o programa pare a execução
+                //de forma "meio por cima", o throw é como um return
+                throw new DomainException("as datas devem ser para depois de hoje");
             }
+            else if (checkout <= checkin)
+            {
+                throw new DomainException("a data de checkOut deve ser para depois da data de checkIn");
+            }
+
+            CheckIn = checkin;
+            CheckOut = checkout;
+        }
+
+        public override string ToString()
+        {
+            return $"Reservation: Room {RoomNumber}, check -in: {CheckIn}, check -out: {CheckOut}, {Duration()} nights";
         }
     }
 }
--- Exception/Exception/Program.cs
+++ Exception/Exception/Program.cs
@@ -2,3 +2,26 @@
 essa seria uma maneira muito ruim por: estar tudo no program, as excessões da classe devem ser tratadas dentro da classe, assim não expondo os tratamentos
 para o programa --//-- poderiamos tratar utilizando if e else fazendo com que retornase uma string dentro da classe, mas isso continuaria ruim msm funcionando, 
 como se trata de excessões podemos e devemos utilizar as Exceptions para tratar erros e excessões para manter a semantica do código, e tudo isso dentro da classe */
+using Exception.ENTITIES;
+
+Console.Write("Room number: ");
+int roomNumber = int.Parse(Console.ReadLine());
+
+Console.Write("Check -in date(dd / MM / yyyy): ");
+DateTime checkInDate = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Check -out date(dd / MM / yyyy): ");
+DateTime checkOuDate = DateTime.Parse(Console.ReadLine());
+
+Reservation reservation = new Reservation(roomNumber, checkInDate, checkOuDate);
+Console.WriteLine(reservation);
+
+Console.WriteLine("");
+Console.WriteLine("Enter data to update the reservation:");
+Console.Write("Check -in date(dd / MM / yyyy): ");
+DateTime newCheckInDate = DateTime.Parse(Console.ReadLine());
+
+Console.Write("Check -out date(dd / MM / yyyy): ");
+DateTime newCheckOuDate = DateTime.Parse(Console.ReadLine());
+reservation = new Reservation(roomNumber, checkInDate, checkOuDate);
+
