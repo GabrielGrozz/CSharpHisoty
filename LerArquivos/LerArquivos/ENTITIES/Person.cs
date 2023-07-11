e0c4152039aae5eb1f4ef67116b1977876278c19
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-07-11 16:12:22 -0300

    remade project based in the proffesor code

---
+++ LerArquivos/LerArquivos/ENTITIES/Person.cs
@@ -0,0 +1,19 @@
+﻿//classe comum
+namespace LerArquivos.ENTITIES
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public int Quantity { get; set; }
+
+        public Person(string name, double value, int quantity)
+        {
+            Name = name;
+            Value = value;
+            Quantity = quantity;
+        }
+
+        public double Total() { return Value * Quantity; }
+    }
+}
---
+++ LerArquivos/LerArquivos/Program.cs
@@ -0,0 +1,47 @@
+﻿using LerArquivos.ENTITIES;
+
+//caminho dos arquivos
+string Sourcepath = @"C:\Riot Games\arquivos\arquivoDeEntrada.csv";
+string Outpath = @"C:\Riot Games\arquivos\arquivoDeSaida.csv";
+
+try
+{
+
+    //array para as linhas de dados pego no sourcePath
+    string[] informacoes = File.ReadAllLines(Sourcepath);
+
+    //laço baseado no tamanho do array dos dados
+    for (int i = 0; i < informacoes.Length; i++)
+    {
+
+        //array que irá conter os dados de cada linha separadamente(separados por ,)
+        string[] data = informacoes[i].Split(',');
+
+        //variaveis com os valores do arquivo
+        string nome = data[0];
+        double valor = double.Parse(data[10]);
+        int quantidade = int.Parse(data[2]);
+
+        //classe instanciada com os dados do arquivo
+        Person pessoa = new Person(nome, valor, quantidade);
+
+        //escrevendo os dados em um novo arquivo
+        using (StreamWriter escritor = File.AppendText(Outpath))
+        {
+            escritor.WriteLine($"{pessoa.Name},{pessoa.Total()}");
+        }
+
+
+    }
+}
+catch(IOException e)
+{
+    Console.WriteLine("[IO EXCEPTION] -- " + e.Message);
+    Console.WriteLine("------------------------------------------");
+}
+catch(Exception e)
+{
+    Console.WriteLine("[GENERIC EXCEPTION] -- " + e.Message);
+    Console.WriteLine("------------------------------------------");
+}
+
