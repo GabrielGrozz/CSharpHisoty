cb1d48ea3d098202fdc0999c72833e9ef4134c60
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-07-18 11:02:01 -0300

    Generics

---
+++ Generics/Generics/ENTITIES/GenericClass.cs
@@ -0,0 +1,105 @@
+﻿
+// ------------ ESSA SERIA A NOSSA CLASSE NORMAL, MAS PRECISAMO DE UMA CLASSE QUE ACEITE TANTO STRING COMO INT, PARA ISSO IREMOS UTILIZAR O GENERICS (<G>) LOGO ABAIXOnamespace Generics.ENTITIES
+//{
+//    internal class GenericClass
+//    {
+
+//        private int[] _values = new int[5];
+//        private int _count = 0;
+//        public void AddValue(int value)
+//        {
+//            if (_count == 5)
+//            {
+//                throw new InvalidOperationException("_values is full");
+//            }
+//            _values[_count] = value;
+//            _count++;
+//        }
+
+//        public int First()
+//        {
+//            if (_count == 0)
+//            {
+//                throw new InvalidOperationException("_values is empty");
+//            }
+//            return _values[0];
+//        }
+
+
+//        public void Print()
+//        {
+//            if (_count == 0)
+//            {
+//                throw new InvalidOperationException("_values is empty");
+//            }
+//            Console.Write("[");
+//            // podemos tanto utilizar o _count como _values.Lenght
+//            for (int i = 0; i < _count - 1; i++)
+//            {
+//                Console.Write(_values[i] + ", ");
+//            }
+//            if (_count > 0)
+//            {
+//                Console.Write(_values[_count - 1]);
+//                Console.WriteLine("]");
+//            }
+
+//        }
+
+//    }
+//}
+
+//-------------- COM GENERICS
+
+namespace Generics.ENTITIES
+{
+    internal class GenericClass<G>
+    {
+        //precisamos passar a partir de agora o tipo que definimos( que no caso é o G) como o tipo dos objetos que vamor utilizar
+
+        private G[] _values = new G[5];
+
+        //como o _count não interfere em nada no tipo que irá ser armazenado no array não é necessário mudar o tipo dele para G
+        private int _count = 0;
+        public void AddValue(G value)
+        {
+            if (_count == 5)
+            {
+                throw new InvalidOperationException("_values is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        public G First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("_values is empty");
+            }
+            return _values[0];
+        }
+
+
+        public void Print()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("_values is empty");
+            }
+            Console.Write("[");
+            // podemos tanto utilizar o _count como _values.Lenght
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0)
+            {
+                Console.Write(_values[_count - 1]);
+                Console.WriteLine("]");
+            }
+
+        }
+
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,44 @@
+﻿
+// ---------------- SE FIZERMOS DESSE JEITO TEREMOS UM PROBLEMA DE REUSO POR CAUSA QUE SÓ IREMOS ACEITAR VALORES DO TIPO INT
+// ---------------- ISSO SERIA UM PROBLEMA CASO QUEIRAMOS USAR NOMES POR EXEMPLO, POR ISSO IREMOS UTILIZAR O GENERICS
+
+// ---------------- PODERIAMO UTILIZAR ELE COM VAR OU OBJECT, MAS TERIAMO UM PROBLEMAS DE TYPE SAFETY
+
+//using Generics.ENTITIES;
+
+//Console.Write("How many values? ");
+
+//GenericClass classeGenerica = new GenericClass();
+
+//int quantity = int.Parse(Console.ReadLine());
+
+//for (int i = 0; i < quantity; i++)
+//{
+//    classeGenerica.AddValue(int.Parse(Console.ReadLine()));
+//}
+
+//classeGenerica.Print();
+//Console.WriteLine("First: " + classeGenerica.First());
+
+
+// -------- USANDO O TIPO GENERIC
+
+using Generics.ENTITIES;
+
+Console.Write("How many values? ");
+
+//quando usamos um tipo Generic na classe precisamos informas qual será o tipo de valor que será usado/armazenado
+//precisamo passar tanto no tipo e na instanciação
+GenericClass<string> classeGenerica = new GenericClass<string>();
+
+int quantity = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < quantity; i++)
+{
+    //caso fosse uma valor do tipo int,double,float... iriamos precisar fazer um Parse
+    classeGenerica.AddValue((Console.ReadLine()));
+}
+
+classeGenerica.Print();
+Console.WriteLine("First: " + classeGenerica.First());
+
