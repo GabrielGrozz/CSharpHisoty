29da670d8ee4fa1433e0d01abbd44925614807bd
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-07-18 21:46:45 -0300

    fix file created

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,2 @@
+﻿// esse projeto é exclusivo para fixacao, caso voce(eu) não lembre como usar algo, reveja a aula e tente aplicar aqui
+// --------------se possível apagar o código após feito, pois ja temos os arquivos com essas bases salvos na pastas com os arquivos
