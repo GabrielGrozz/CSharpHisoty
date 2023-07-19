73a97360bb076508f2f4cc3ad46ffe822f81710b
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-07-19 17:33:23 -0300

    fix

---
+++ ConsoleApp3/ConsoleApp3/Entities/PrivateService.cs
@@ -0,0 +1,50 @@
+﻿namespace ConsoleApp3.Entities
+{
+    //essa seria uma classe normal, sem generics, sendo assim ela só aceita valores do tipo int, mas podemos fazer com que ela aceite outro tipos de valores
+    //só iremos precisar transformala em uma classe generica como o GEnerics, que seria com o <g>
+    //isso nos dara a possibilidadde de reuso
+
+    //veja abaixo a classe de modo genetico
+    internal class PrivateService<G>
+    {
+        //aqui iremos utilizar somente a letra
+        //isso fara com que o valor que será retornar possa variar entro outro tipos, sejam eles string, number...
+        private G[] _values = new G[10];
+        private int _count = 0;
+
+        //funcao que adiciona um valor no array
+        public void AddValue(G value)
+        {
+            if (_count == 10)
+            {
+                throw new InvalidOperationException("array is full");
+            }
+            _values[_count] = value;
+            _count++;
+        }
+
+        //retorna o primeiro valor
+        public G First()
+        {
+            if (_count == 0)
+            {
+                throw new InvalidOperationException("array is empty");
+            }
+            return _values[0];
+        }
+
+        //mostrado todos os valores
+        public void Print()
+        {
+            Console.Write('[');
+            for (int i = 0; i < _count - 1; i++)
+            {
+                Console.Write(_values[i] + ", ");
+            }
+            if (_count > 0){
+            Console.WriteLine(_values[_count - 1] + "]");
+            }
+
+        }
+    }
+}
--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,2 +1,28 @@
 ﻿// esse projeto é exclusivo para fixacao, caso voce(eu) não lembre como usar algo, reveja a aula e tente aplicar aqui
 // --------------se possível apagar o código após feito, pois ja temos os arquivos com essas bases salvos na pastas com os arquivos
+
+// ----------- GENERICS
+
+using ConsoleApp3.Entities;
+
+//precisamo passar o tipo de valor que será a nossa classe com os <tipoQueSeraUsado>
+PrivateService<string> privateService = new PrivateService<string>();
+
+Console.Write("how many values? ");
+int quantity = int.Parse(Console.ReadLine());
+for (int i = 0; i < quantity; i++)
+{
+    //privateService.AddValue(int.Parse(Console.ReadLine()));
+    privateService.AddValue(Console.ReadLine());
+
+}
+
+Console.WriteLine(" ");
+Console.Write("first: ");
+Console.WriteLine(privateService.First());
+Console.WriteLine(" ");
+Console.WriteLine(" ");
+
+privateService.Print();
+
+
