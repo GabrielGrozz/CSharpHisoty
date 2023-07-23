b1084b06894f215ef6e2259fe20deb1a2bf72b46
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-07-22 21:38:36 -0300

    sorted

---
+++ GetHash/GetHash/Program.cs
@@ -0,0 +1,22 @@
+﻿//Hashset seria outra forma de armazernarmos valores, segue a mesmo sintaxe do List, porém ele não aceita armazenar valores repetidos e os valores armazenador não possuem uma posição
+//por não possuir possições não é possível fazer um laço for nele, mas podemos utilizar o foreach
+
+HashSet<string> values = new HashSet<string>();
+
+values.Add("gabriel");
+values.Add("davi");
+//mesmo passando para adicioanr dois leo, só será adicionado um
+values.Add("leo");
+values.Add("rtx4090");
+values.Add("leo");
+values.Add("controle8bitdo");
+
+
+    //podemos verificar se existe um elemento dentro dele atravez do contains, que irá nos retornar um booleano
+    Console.WriteLine(values.Contains("garrafaPET"));
+    Console.WriteLine(values.Contains("gabriel"));
+
+foreach (var value in values) { 
+
+    Console.WriteLine(value);
+}
---
+++ sortedset/sortedset/Program.cs
@@ -0,0 +1,23 @@
+﻿//diferente do hashset, o sortedset é organizado
+
+SortedSet<int> a = new SortedSet<int>() { 41, 451, 784, 5154, 165, 417, 54, 132, 156 };
+SortedSet<int> b = new SortedSet<int>() { 41, 451, 784, 4, 1, 89, 6, 2, 7, 75 };
+
+//podemos instanciar um sortedset com dados de outo sortedset
+SortedSet<int> c = new SortedSet<int>(a);
+
+//podemos unir dos sortedset, mas os valores que se repetem não irão ser adicionados
+c.UnionWith(b);
+
+//podemos pegar a intercequeção dos dois conjuntos, ou seja, os numeros que se repetem
+SortedSet<int> d = new SortedSet<int>(a);
+d.IntersectWith(c); 
+
+//podemos criar um sorted com os valores, menos os valores de outro passado
+SortedSet<int> e = new SortedSet<int>(a);
+e.ExceptWith(b);
+
+
+
+
+
