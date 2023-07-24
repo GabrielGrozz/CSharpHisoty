4c0fbeb1a958223a96b49248a7a8043f6609c6dc
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-07-24 17:58:17 -0300

    ad

--- Comparison/Comparison/Program.cs
+++ Comparison/Comparison/Program.cs
@@ -19,7 +19,7 @@ products.Add(product4);
 
 //podemos também usar uma expressão lambda, que seria uma callback do js
 //não precisamo passar o tipo dos parametro, pois eles ja são passados implicitamente
-products.Sort((p1, p2) => p1.Name.CompareTo(p2.Name));
+products.Sort((p1, p2) => p1.Name.CompareTo(p2.Name)); 
 
 foreach (Product product in products) Console.WriteLine(product.Name + " " + product.Value);
 
