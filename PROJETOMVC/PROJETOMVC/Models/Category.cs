b7835879b4aa1631a28660858cb96deacedba401
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-08 17:42:39 -0300

    project

---
+++ PROJETOMVC/PROJETOMVC/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace PROJETOMVC.Models
+{
+    //essa será a nossa classe de categorias, uma catogira terá muitos lanches
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; } 
+
+        List<Lunch> Lunchs { get; set; }
+    }
+}
