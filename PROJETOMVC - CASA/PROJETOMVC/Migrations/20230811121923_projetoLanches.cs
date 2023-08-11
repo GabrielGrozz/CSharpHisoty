41b96cf6027684bc70f637b29998070f3d487428
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-11 09:26:04 -0300

    added a project to home and other to work

---
+++ PROJETOMVC - CASA/PROJETOMVC/Migrations/20230811121923_projetoLanches.cs
@@ -0,0 +1,246 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PROJETOMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class projetoLanches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lunchs_Categories_CategoryId",
+                table: "Lunchs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Lunchs",
+                table: "Lunchs");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Lunchs",
+                newName: "Lanches");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "Categorias");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lunchs_CategoryId",
+                table: "Lanches",
+                newName: "IX_Lanches_CategoryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Lanches",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Lanches",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Lanches",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LongDescription",
+                table: "Lanches",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Lanches",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageThumbUR",
+                table: "Lanches",
+                type: "nvarchar(250)",
+                maxLength: 250,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categorias",
+                type: "nvarchar(150)",
+                maxLength: 150,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Categorias",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Lanches",
+                table: "Lanches",
+                column: "LunchId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lanches_Categorias_CategoryId",
+                table: "Lanches",
+                column: "CategoryId",
+                principalTable: "Categorias",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Lanches_Categorias_CategoryId",
+                table: "Lanches");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Lanches",
+                table: "Lanches");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categorias",
+                table: "Categorias");
+
+            migrationBuilder.RenameTable(
+                name: "Lanches",
+                newName: "Lunchs");
+
+            migrationBuilder.RenameTable(
+                name: "Categorias",
+                newName: "Categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Lanches_CategoryId",
+                table: "Lunchs",
+                newName: "IX_Lunchs_CategoryId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ShortDescription",
+                table: "Lunchs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Lunchs",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Lunchs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LongDescription",
+                table: "Lunchs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageURL",
+                table: "Lunchs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageThumbUR",
+                table: "Lunchs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(250)",
+                oldMaxLength: 250);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(150)",
+                oldMaxLength: 150);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Categories",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Lunchs",
+                table: "Lunchs",
+                column: "LunchId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Lunchs_Categories_CategoryId",
+                table: "Lunchs",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
