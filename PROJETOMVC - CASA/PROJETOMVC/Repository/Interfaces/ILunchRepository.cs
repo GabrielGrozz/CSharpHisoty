4c13b7383a68ab51fae78af91c640d564e3d117a
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-14 10:35:40 -0300

    reposity lunch

---
+++ PROJETOMVC - CASA/PROJETOMVC/Repository/Interfaces/ILunchRepository.cs
@@ -0,0 +1,11 @@
+﻿using PROJETOMVC.Models;
+
+namespace PROJETOMVC.Repository.Interfaces
+{
+    public interface ILunchRepository
+    {
+        public IEnumerable<Lunch> Lanches { get; }
+        public IEnumerable<Lunch> LanchesPreferidos { get; }
+        public Lunch GetLancheByID(int LancheID);
+    }
+}
---
+++ PROJETOMVC - CASA/PROJETOMVC/Repository/LunchRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PROJETOMVC.Context;
+using PROJETOMVC.Models;
+using PROJETOMVC.Repository.Interfaces;
+
+namespace PROJETOMVC.Repository
+{
+    public class LunchRepository : ILunchRepository
+    {
+        private readonly AppDbContext _context;
+
+        LunchRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Lunch> Lanches => _context.Lunchs.Include(e => e.Category);
+
+        public IEnumerable<Lunch> LanchesPreferidos => _context.Lunchs.Where(e => e.Preferred).Include(e => e.Category);
+
+        public Lunch GetLancheByID(int id)
+        {
+            return _context.Lunchs.FirstOrDefault(e => e.LunchId == id);
+        }
+    }
+}
