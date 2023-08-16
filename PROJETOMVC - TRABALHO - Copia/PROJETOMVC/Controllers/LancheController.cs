7d9c534faa51324d10a28468f3ba255890d95bbd
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-16 11:03:43 -0300

    add view model

--- PROJETOMVC - TRABALHO - Copia/PROJETOMVC/Controllers/LancheController.cs
+++ PROJETOMVC - TRABALHO - Copia/PROJETOMVC/Controllers/LancheController.cs
@@ -3,6 +3,7 @@ using Microsoft.Identity.Client;
 using Microsoft.IdentityModel.Tokens;
 using PROJETOMVC.Repository;
 using PROJETOMVC.Repository.Interfaces;
+using PROJETOMVC.ViewModels;
 
 namespace PROJETOMVC.Controllers
 {
@@ -17,8 +18,20 @@ namespace PROJETOMVC.Controllers
 
         public IActionResult List()
         {
-            var lanches = _lunchRepository.Lanches;
-            return View(lanches);
+            //var lanches = _lunchRepository.Lanches;
+            //return View(lanches);
+
+            //iremos utilizar viewmodel para que possamos pegar e exibir somente os dados necessários
+            //iremo instanciar a classe da viewmodel
+            var lunchListViewModel = new LunchListViewModel();
+            //usamos a instancia que possui uma lista do tipo lanches e adicionamos os lanches de _lunchRepository nela
+            lunchListViewModel.Lanches = _lunchRepository.Lanches;
+            //setamos a categoria também
+            lunchListViewModel.CategoriaAtual = "categoriaaaaakkkkkkkkkk";
+
+            //com a view model nós unimos somente as propriedade que vamos usar do lanche e categoria
+            //passamos para a view
+            return View(lunchListViewModel);
         }
     }
 }
---
+++ PROJETOMVC - TRABALHO - Copia/PROJETOMVC/ViewModels/LunchListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using PROJETOMVC.Models;
+
+namespace PROJETOMVC.ViewModels
+{
+    //esse será a nossa view model
+    public class LunchListViewModel
+    {
+        public IEnumerable<Lunch> Lanches { get; set; }
+        public string CategoriaAtual { get; set; }
+    }
+}
