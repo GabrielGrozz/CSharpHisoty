4f98e865e77d5b2a16f48e2371d1d4cafb801157
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-16 16:46:02 -0300

    add CarrinhoDeComprar on DB

--- PROJETOMVC - TRABALHO/PROJETOMVC/Migrations/20230816192737_CarrinhoCompraItem.Designer.cs
+++ PROJETOMVC - TRABALHO/PROJETOMVC/Migrations/20230816192737_CarrinhoCompraItem.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PROJETOMVC.Context;
 
@@ -10,9 +11,11 @@ using PROJETOMVC.Context;
 namespace PROJETOMVC.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230816192737_CarrinhoCompraItem")]
+    partial class CarrinhoCompraItem
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
