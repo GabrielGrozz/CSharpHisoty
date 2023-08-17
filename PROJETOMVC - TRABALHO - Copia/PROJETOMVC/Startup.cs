701246a9082430fd9b4089a60cf4c92b9e37dad4
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-17 10:25:50 -0300

    configured session Middleware

--- PROJETOMVC - TRABALHO - Copia/PROJETOMVC/Startup.cs
+++ PROJETOMVC - TRABALHO - Copia/PROJETOMVC/Startup.cs
@@ -15,9 +15,16 @@ public class Startup
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
-    {
+    {   //configurando o middleware
+        services.AddMemoryCache();
+        services.AddSession();
+
+
         services.AddTransient<ILunchRepository, LunchRepository>();
         services.AddTransient<ICategoryRepository, CategoryRepository>();
+
+        //registrando o serviço para termos acesso aos dados da requisição
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
         services.AddDbContext<AppDbContext>(op =>
         op.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
@@ -26,7 +33,9 @@ public class Startup
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-    {
+    {   //ativando o middleware
+        app.UseSession();
+
         if (env.IsDevelopment())
         {
             app.UseDeveloperExceptionPage();
