3cadbfb601188168dc665e3933a0a50eda2f8249
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-22 11:02:14 -0300

    TahHelpers outline

---
+++ PROJETOMVC - TRABALHO - oficial/PROJETOMVC/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace PROJETOMVC.TagHelpers
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public string Endereco { get; set; }
+        public string COnteudo { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            base.Process(context, output);
+        }
+    }
+}
