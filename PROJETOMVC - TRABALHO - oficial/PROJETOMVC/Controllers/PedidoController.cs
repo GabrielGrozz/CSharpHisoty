eb3b0ce5bca153092d33d5ac8a2005152f25719b
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-08-28 10:41:36 -0300

    making checkout view

--- PROJETOMVC - TRABALHO - oficial/PROJETOMVC/Controllers/PedidoController.cs
+++ PROJETOMVC - TRABALHO - oficial/PROJETOMVC/Controllers/PedidoController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LanchesMac.Models;
+using Microsoft.AspNetCore.Mvc;
+using PROJETOMVC.Repository.Interfaces;
 
 namespace PROJETOMVC.Controllers
 {
     public class PedidoController : Controller
     {
-        public IActionResult Index()
+        private readonly IPedidoRepository _pedidoRepository;
+        public PedidoController(IPedidoRepository pedidoRepository)
         {
+            _pedidoRepository = pedidoRepository;
+        }
+
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Checkout(Pedido pedido) 
+        { 
             return View();
         }
     }
