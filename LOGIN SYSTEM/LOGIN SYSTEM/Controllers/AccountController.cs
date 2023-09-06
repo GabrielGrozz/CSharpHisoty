9004006f55e48ab27fdf550024b1e2cc5caaa46a
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-09-06 17:32:57 -0300

    add login controller and model

---
+++ LOGIN SYSTEM/LOGIN SYSTEM/Controllers/AccountController.cs
@@ -0,0 +1,58 @@
+﻿using LOGIN_SYSTEM.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOGIN_SYSTEM.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        //essa será a nossa view onde iremos efetuar o login
+        [HttpGet]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel
+            {
+                ReturnUrl = returnUrl,
+            });
+        }
+
+        [HttpPost]
+        //                                      iremos receber o parametro quando confirmarmos o formulario
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+        {
+            //aqui verificamos se o model está valido
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+
+            //pegamos o nome do usuario no banco
+            var user = await _userManager.FindByNameAsync(loginViewModel.UserName);
+
+            //se o nome for diferente de null 
+            if(user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+                if (result.Succeeded)
+                {
+                    //verifica se´tem uma página de retorno
+                    if (string.IsNullOrEmpty(loginViewModel.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(loginViewModel.ReturnUrl);
+                }
+            }
+            //caso o usuario seja null
+            ModelState.AddModelError("", "Falhar ao realizar login");
+            return View(loginViewModel);
+        }
+    }
+}
---
+++ LOGIN SYSTEM/LOGIN SYSTEM/Models/LoginViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LOGIN_SYSTEM.Models
+{
+    public class LoginViewModel
+    {
+        //essa sera a nossa model para os dados do usuário
+
+        [Required(ErrorMessage = "informe um nome")]
+        [Display(Name = "Nome do usuário")]
+        public string UserName { get; set; }
+
+        [Required(ErrorMessage = "informe uma senha")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Senha do usuário")]
+        public string Password { get; set; }
+        
+        //a returnurl é a url da ultima página que abrimos
+        public string ReturnUrl { get; set; }
+    }
+}
