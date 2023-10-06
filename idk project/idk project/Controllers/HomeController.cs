0e8600577b236185b1db7d1a7f5d740f2a87b3e2
GabrielGrozz <gabrielgrozz.net@gmail.com>
2023-10-06 17:01:27 -0300

    create controller and views to do a CRUD

---
+++ idk project/idk project/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using idk_project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace idk_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ idk project/idk project/Controllers/PokedexController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokeApiNet;
+
+namespace idk_project.Controllers
+{
+    public class PokedexController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var Client = new PokeApiClient();
+            Pokemon Hoho = await Client.GetResourceAsync<Pokemon>("ho-ho");
+            Console.WriteLine(Hoho);
+            return View(Hoho);
+        }
+
+    }
+}
